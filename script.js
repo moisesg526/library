8faebf1cccdf31ea047f814cfbafe959956c06c4
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-06-26 23:51:06 -0400

    Added a css file, an array for the library books, also wrote a constructor function for the books, also added a function to add books to the library

---
+++ script.js
@@ -0,0 +1,23 @@
+const library = [
+  "Ellie's spooky surprise",
+  "Mercy Watson goes for a ride",
+  "Marion and the secret letter",
+  "Heidi Heckelbeck might be afraid of the dark",
+  "Heidi Heckelbeck and the hair emergency!",
+];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    console.log(`${this.title} by ${this.author} ${this.pages}, ${this.read}`);
+    return `${this.title} by ${this.author} ${this.pages}, ${this.read}`;
+  };
+  console.log("book");
+}
+
+function addBookToLibrary() {
+  console.log("Book Added");
+}
